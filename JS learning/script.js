435a0323177b7275b407af464d41639dc069fd82
Labiba Sanowar <labibasanowerk@gmail.com>
2026-09-01 16:58:50 +0600

    Started learning JavaScript

---
+++ JS learning/script.js
@@ -0,0 +1,28 @@
+alert('Hello World!');
+
+console.log('Finally learning JavaScript!');
+
+console.log('Lessgooooo');
+
+console.log('Hope it\'s fun!');
+
+var a = prompt('what is your id?');
+
+console.log('Hello, ' + a + '!');
+
+var b = prompt('what is your password?');
+
+console.log('Your password is: ' + b);
+
+var istrue = confirm('Are you sure you want to continue?');
+
+if (istrue) {
+    console.log('You have confirmed to continue.');
+} 
+else {
+    console.log('You have cancelled the operation.');
+}
+
+document.title = 'JS lessons';
+
+document.body.style.backgroundColor = 'lightblue';
