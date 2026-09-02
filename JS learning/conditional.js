84a47ecdff972e37aaa9210cc7990b09a300812d
Labiba Sanowar <labibasanowerk@gmail.com>
2026-09-02 23:54:08 +0600

    Learnt conditionals in JS

---
+++ JS learning/conditional.js
@@ -0,0 +1,53 @@
+console.log('Learning conditionals');
+
+let marks = 56;
+let practical_marks = 23;
+
+// if ((marks + practical_marks) >= 80) {
+//     console.log('You scored ' + (marks + practical_marks));
+//     console.log('You have an A+ grade');
+// }
+
+// else {
+//     console.log('You scored ' + (marks + practical_marks));
+//     console.log('better luck next time');
+// }
+
+let grade = 2;
+
+// console.log( age + grade);
+// console.log( age - grade);
+// console.log( age * grade);
+// console.log( age / grade);
+// console.log( age ** grade);
+// console.log( age % grade);
+
+// age += grade;
+
+// console.log(age);
+
+// if (age >= 18) {
+//     console.log('You are eligible to vote');
+// }
+
+// else {
+//     console.log('You are not eligible to vote');
+// }
+
+let age = 18;
+
+if (age == 18) {
+    console.log('You are good to go');
+}
+
+else if (age == 0) {
+    console.log('Bruh fr?');
+}
+
+else if (age == 1) {
+    console.log('Stop kidding');
+}
+
+else {
+    console.log('bye bye');
+}
