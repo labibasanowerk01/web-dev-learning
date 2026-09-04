dd94b5ba2dc7a722b71e86df12491257994c7c54
Labiba Sanowar <labibasanowerk@gmail.com>
2026-09-04 14:03:23 +0600

    Learnt JS strings

---
+++ JS learning/strings.js
@@ -0,0 +1,40 @@
+console.log("I'm learning strings today");
+
+let  a = "Labiba";
+
+console.log(a);
+
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+// console.log(a[6]);
+
+console.log(a.length);
+
+let myname = "Labiba";
+
+let friend = "Eric";
+
+console.log("Her name is " + myname + " and her friend is " + friend);
+// Easier way
+// Template literals
+console.log(`Her name is ${myname} and her friend is ${friend}`);
+
+let b = "Samiha";
+
+console.log(b.length);
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.slice(0,4));
+console.log(b.slice(1));
+console.log(b.replace("h", "y"));
+console.log(b.concat(myname, "2719"));
+
+// removes whitespaces
+
+// let b2 = b.trim()
+
+// console.log(b2);
