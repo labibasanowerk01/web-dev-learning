f59c386a5155f208e6c99607210edf5c1bb1194d
Labiba Sanowar <labibasanowerk@gmail.com>
2026-09-10 23:37:13 +0600

    Learnt JS Children,Parents and Sibling nodes

---
+++ JS learning/children-parents-sibling-nodes.js
@@ -0,0 +1,7 @@
+console.log("Hello world")
+
+document.body.firstElementChild 
+document.body.firstElementChild.childNodes 
+document.body.firstElementChild.children
+document.body.firstElementChild.children[3].nextElementSibling
+document.body.firstElementChild.children[3].previousElementSibling
