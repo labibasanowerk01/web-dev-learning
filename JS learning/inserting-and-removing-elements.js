1f80f5c98291a5df254c5591016ab24a47def43a
Labiba Sanowar <labibasanowerk@gmail.com>
2026-09-13 23:27:51 +0600

    Started to learn how to insert or remove elements using JS

---
+++ JS learning/inserting-and-removing-elements.js
@@ -0,0 +1,11 @@
+document.body.querySelector('.container').innerHTML
+document.body.querySelector('.container').innerText
+document.body.querySelector('.container').outerHTML
+document.body.querySelector('.container').tagName
+// document.body.querySelector('.container').hidden = true
+document.body.querySelector('.box').attributes
+document.body.querySelector('.box').hasAttribute('style')
+document.body.querySelector('.box').getAttribute('style')
+document.body.querySelector('.box').setAttribute('style', 'background-color: blue')
+document.body.querySelector('.box').removeAttribute('style')
+document.designMode ="on"
