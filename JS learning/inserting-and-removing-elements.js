88a0aa8af3564af141654bcd4e67877f4d4d89b0
Labiba Sanowar <labibasanowerk@gmail.com>
2026-09-14 16:09:41 +0600

    Learnt Inserting and Removing elements through JS

--- JS learning/inserting-and-removing-elements.js
+++ JS learning/inserting-and-removing-elements.js
@@ -9,3 +9,28 @@ document.body.querySelector('.box').getAttribute('style')
 document.body.querySelector('.box').setAttribute('style', 'background-color: blue')
 document.body.querySelector('.box').removeAttribute('style')
 document.designMode ="on"
+document.body.querySelector('.box').dataset
+
+// let div = document.createElement('div')
+
+// div.setAttribute('class','js-box')
+
+// div.innerHTML = "I am a box created by JS"
+
+// document.querySelector('.container').append(div)
+
+let cont = document.querySelector('.container')
+
+cont.insertAdjacentHTML("beforeend", "Cristo Ronaldo SIUUUUUU")
+
+document.querySelector('.box').remove()
+
+document.querySelector(".container").classList
+
+document.querySelector(".container").className
+
+document.querySelector(".container").classList.add('mama-box')
+
+document.querySelector(".container").classList.remove('papa-box')
+
+document.querySelector(".container").classList.toggle('og')
