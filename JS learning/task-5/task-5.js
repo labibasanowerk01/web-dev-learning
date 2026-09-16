9f025295cda514ffd9f9e8e049681ed76b7a838c
Labiba Sanowar <labibasanowerk@gmail.com>
2026-09-16 16:23:47 +0600

    Turned it into a random card generator

--- JS learning/task-5/task-5.js
+++ JS learning/task-5/task-5.js
@@ -23,11 +23,62 @@ function createCard(title, cName, views, monthsOld, duration, thumbnail) {
             document.querySelector(".container").innerHTML = document.querySelector(".container").innerHTML + html
 }
 
-createCard(
-    "Introduction to Frontend | Sigma web dev video #3",
-    "CodeWithHarry",
-    450000,
-    "1 month ago",
-    "28:15",
-    "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw"
-)
+let crc = [function () {
+    createCard(
+        "Introduction to Frontend | Sigma web dev video #1",
+        "CodeWithHarry",
+        450000,
+        "1 month ago",
+        "28:15",
+        "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw"
+    )},
+    function () {
+    createCard(
+        "Introduction to Backend | Sigma web dev video #2",
+        "CodeWithHarry",
+        610000,
+        "1 month ago",
+        "28:15",
+        "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw"
+    )
+},
+    function () {
+    createCard(
+        "Introduction to AI | Sigma web dev video #3",
+        "CodeWithHarry",
+        236783,
+        "1 month ago",
+        "28:15",
+        "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw"
+    )
+},
+    function () {
+    createCard(
+        "Introduction to Machine learning | Sigma web dev video #4",
+        "CodeWithHarry",
+        560000,
+        "1 month ago",
+        "28:15",
+        "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw"
+    )
+},
+    function () {
+    createCard(
+        "Introduction to Databases | Sigma web dev video #5",
+        "CodeWithHarry",
+        120000,
+        "1 month ago",
+        "28:15",
+        "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw"
+    )
+},
+
+]
+
+
+let btn = document.querySelector('.btn');
+
+btn.addEventListener('click', function () {
+        let randomIndex = Math.floor(Math.random() * crc.length);
+        crc[randomIndex]();
+});
