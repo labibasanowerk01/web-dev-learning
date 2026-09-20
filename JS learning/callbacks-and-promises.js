e401abae95613e6755efbc3413233848583c9670
Labiba Sanowar <labibasanowerk@gmail.com>
2026-09-20 23:08:50 +0600

    Strarted learning Promises

--- JS learning/callbacks-and-promises.js
+++ JS learning/callbacks-and-promises.js
@@ -11,5 +11,15 @@ loadscript (callback)
 console.log (callback ('lol'))
 
 
+let prom1 = new Promise((resolve, reject) => {
+setInterval(() => {
+    console.log('Hello');
+    resolve ('Bye')
+}, 3000);
 
+})
 
+prom1.then ((a)=>{
+    console.log(a);
+
+})
